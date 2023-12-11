2b0904cfd818ea9651c10e0846525275b82acaed
Divya R <kwrdivyaraj100@gmail.com>
2023-12-11 15:37:28 +0530

    Adding header and routes for blog app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    }
+  }
   render() {
     return (
-      <div >
-        <Header />
-      </div>
+      <>
+        <header>
+          <Header isLoggedIn={this.state.isLoggedIn} />
+        </header>
+        <main>
+          <Routes>
+            <Route path='/auth' element={<Login />} />
+            <Route path='/blogs' element={<Blogs />} />
+            <Route path='/myBlogs' element={<UserBlogs />} />
+            <Route path='/myBlogs/:id' element={<BlogDetail />} />
+            <Route path='/blogs/add' element={<AddBlogs />} />
+          </Routes>
+        </main>
+      </>
     );
   }
 }
