b5f41f479f340748cb91a97b19b48de53fcc9bdf
Divya R <kwrdivyaraj100@gmail.com>
2023-12-12 13:18:53 +0530

    Implementing state management and form handling for user inputs

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
 
 export class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputs: {
+                name: "",
+                email: "",
+                password: ""
+            }
+
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState((prevState) => ({
+            inputs: {
+                ...prevState.inputs,
+                [e.target.name]: e.target.value,
+            }
+        }))
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        console.log(this.state.inputs);
+    }
     render() {
         return (
             <div>Signup</div>
