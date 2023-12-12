c1b3678d286f6aa99bafd6ee80ef49ecdc9f4ed4
Divya R <kwrdivyaraj100@gmail.com>
2023-12-12 15:05:40 +0530

    Adding dynamic background image to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 
 export class Header extends Component {
-    constructor(props) {
-        super(props);
-      }
-    render() {
-        let imageURL = `url(${this.props.banner})`;
-        return (
-            <>
-            <header className="masthead" style={{ backgroundImage: { imageURL } }}>
-              <div className="container position-relative px-4 px-lg-5">
-                <div className="row gx-4 gx-lg-5 justify-content-center">
-                  <div className="col-md-10 col-lg-8 col-xl-7">
-                    <div className="page-heading">
-                      <h1>{this.props.heading}</h1>
-                      <span className="subheading">{this.props.subHeading}</span>
-                    </div>
-                  </div>
-                </div>
+
+  render() {
+    let imageURL = `url('${this.props.banner}')`;
+    console.log(imageURL); return (
+      <>
+        <header className="masthead" style={{ "backgroundImage": { imageURL } }}>              <div className="container position-relative px-4 px-lg-5">
+          <div className="row gx-4 gx-lg-5 justify-content-center">
+            <div className="col-md-10 col-lg-8 col-xl-7">
+              <div className="page-heading">
+                <h1>{this.props.heading}</h1>
+                <span className="subheading">{this.props.subHeading}</span>
               </div>
-            </header>
-          </>
-        )
-    }
+            </div>
+          </div>
+        </div>
+        </header>
+      </>
+    )
+  }
 }
 
 export default Header;
