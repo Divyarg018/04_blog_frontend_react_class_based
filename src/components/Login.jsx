10f169cf1906bc19227723267732f07a4aa1e555
Divya R <kwrdivyaraj100@gmail.com>
2023-12-12 11:54:55 +0530

    creating the HTML template for the blog frontend, using React class components and Bootstrap styles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 export class Login extends Component {
+ 
+   
     render() {
         return (
-            <div>Login</div>
+          <div>
+            Login
+            </div>
         )
     }
 }
