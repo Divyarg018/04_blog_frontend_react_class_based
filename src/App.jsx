43e74b6d83a68bceaf4f116df47e39c27ea8db9c
Divya R <kwrdivyaraj100@gmail.com>
2023-12-12 16:14:33 +0530

    Importing new components in main file

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,33 @@ import Login from './components/Login';
 import Logout from './components/Logout';
 import Signup from './components/Signup';
 import Home from './components/Home';
+import AddBlogs from './components/AddBlogs';
+import Blogs from './components/Blogs';
+import UserBlogs from './components/UserBlogs';
+import BlogDetail from './components/BlogDetail';
 
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       isLoggedIn: false,
     }
   }
   render() {
-    
+
     return (
       <>
-       <Navbar isLoggedIn={this.state.isLoggedIn}/>
+        <Navbar isLoggedIn={this.state.isLoggedIn} />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/auth' element={<Login />}></Route>
-          <Route path='/auth/logout' element={<Logout />}></Route>
-          <Route path='/auth/signup' element={<Signup />}></Route>
+          <Route path='/auth' element={<Login />} />
+          <Route path='/auth/logout' element={<Logout />} />
+          <Route path='/auth/signup' element={<Signup />} />
+
+          <Route path='/blogs' element={<Blogs />} />
+          <Route path='/blog/add' element={<AddBlogs />} />
+          <Route path='/myBlogs' element={<UserBlogs />} />
+          <Route path='/myBlogs/:id' element={<BlogDetail />} />
         </Routes>
         <Footer />
 
