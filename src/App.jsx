ec5dd52c4ad9f83c21bf3a922153974a80d9b766
Divya R <kwrdivyaraj100@gmail.com>
2023-12-12 12:39:32 +0530

    Adding Header component to React app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import Header from './components/Header';
 import Footer from './components/Footer';
 
 export default class App extends Component {
