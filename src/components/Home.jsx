0c5aeb218169b7863a2ba78ded6e1376044501de
Divya R <kwrdivyaraj100@gmail.com>
2023-12-12 17:15:35 +0530

    Adding async data fetching in React component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import Blog from './Blog';
+import axios from 'axios';
 
 export class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            blogs: [],
+        }
+        this.sendRequest = this.sendRequest.bind(this);
+    }
+
+    async sendRequest() {
+        const res = await axios.get(`http://localhost:5000/api/blog`)
+            .catch(err => console.log(err))
+
+        const data = await res.data;
+        //console.log(data);
+        return data;
+    }
+    async componentDidMount() {
+        this.sendRequest().then(data => this.setState(data));
+        //console.log(data.blogs);
+        //this.setState({ blogs: data.blogs });
+    }
     render() {
         return (
             <>
-    <header className="masthead" style={{ "backgroundImage": "url('assets/img/home.jpg')" }}>
+                <header className="masthead" style={{ "backgroundImage": "url('assets/img/home.jpg')" }}>
                     <div className="container position-relative px-4 px-lg-5">
                         <div className="row gx-4 gx-lg-5 justify-content-center">
                             <div className="col-md-10 col-lg-8 col-xl-7">
