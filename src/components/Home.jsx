981eed4c2354fc203f356089e17ee82ac218b0e3
Divya R <kwrdivyaraj100@gmail.com>
2023-12-12 15:08:31 +0530

    Updating header component with new styling and content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,18 @@ export class Home extends Component {
     render() {
         return (
             <>
-                <Header banner="/assets/img/home.jpg" heading="Filmy BLog" subHeading="Welcome to Chitraloka" />
-                <div className="container px-4 px-lg-5">
+    <header className="masthead" style={{ "backgroundImage": "url('assets/img/home.jpg')" }}>
+                    <div className="container position-relative px-4 px-lg-5">
+                        <div className="row gx-4 gx-lg-5 justify-content-center">
+                            <div className="col-md-10 col-lg-8 col-xl-7">
+                                <div className="page-heading">
+                                    <h1>Filmy Blog</h1>
+                                    <span className="subheading">Welcome to Loka...Chitraloka</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>                <div className="container px-4 px-lg-5">
                     <div className="row gx-4 gx-lg-5 justify-content-center">
                         <div className="col-md-10 col-lg-8 col-xl-7">
 
