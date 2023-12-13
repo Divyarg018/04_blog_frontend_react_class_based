c70c4747ee8978ae811d18f46796fee1ee0424a3
Divya R <kwrdivyaraj100@gmail.com>
2023-12-13 15:17:55 +0530

    Styling style.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/Home.jsx';
-import Footer from './components/Footer';
-import Login from './components/Login';
-import { Route, Routes } from 'react-router-dom';
-import Logout from './components/Logout';
-import Signup from './components/Signup';
-import AddBlogs from './components/AddBlogs';
-import UserBlogs from './components/UserBlogs';
-import BlogDetail from './components/BlogDetail';
+import React, { Component } from 'react'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import { Route, Routes } from 'react-router-dom'
+import Login from './components/Login'
+import Logout from './components/Logout'
+import Signup from './components/Signup'
+import AddBlogs from './components/AddBlogs'
+import UserBlogs from './components/UserBlogs'
+import BlogDetail from './components/BlogDetail'
+import Blog from './components/Blog'
 
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn: false,
+      isLoggedIn: true,
     }
     this.setLoggedIn = this.setLoggedIn.bind(this);
   }
@@ -26,17 +27,17 @@ export class App extends Component {
   render() {
     return (
       <>
-        <Navbar isLoggedIn={this.state.isLoggedIn} />
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
           <Route path='/auth' element={<Login setLoggedIn={this.setLoggedIn} />} />
-          <Route path='/auth/logout' element={<Logout setLoggedIn={this.setLoggedIn()} />} />
-          <Route path='/auth/signup' element={<Signup setLoggedIn={this.setLoggedIn()} />} />
+          <Route path='/auth/logout' element={<Logout setLoggedIn={this.setLoggedIn} />} />
+          <Route path='/auth/signup' element={<Signup setLoggedIn={this.setLoggedIn} />} />
 
           <Route path='/' element={<Home />} />
+          <Route path='/blog/:id' element={<Blog />} />
           <Route path='/blog/add' element={<AddBlogs isLoggedIn={this.state.isLoggedIn} />} />
           <Route path='/myBlogs' element={<UserBlogs />} />
-          <Route path='/myBlogs/:id' element={<BlogDetail />} />
+          <Route path='/editBlog/:id' element={<BlogDetail />} />
         </Routes>
         <Footer />
 
