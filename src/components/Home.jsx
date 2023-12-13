1bce65e2759c779e80e4e0e4d9413d03413b96dd
Divya R <kwrdivyaraj100@gmail.com>
2023-12-13 12:05:14 +0530

    Adding Home component with dynamic blog display and API calls

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Blog from './Blog';
 import axios from 'axios';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export class Home extends Component {
     constructor(props) {
@@ -28,27 +27,47 @@ export class Home extends Component {
     render() {
         return (
             <>
-                <header className="masthead" style={{ "backgroundImage": "url('assets/img/home.jpg')" }}>
+                <header className="masthead" style={{ "backgroundImage": "url('assets/img/about.jpg')" }}>
                     <div className="container position-relative px-4 px-lg-5">
                         <div className="row gx-4 gx-lg-5 justify-content-center">
                             <div className="col-md-10 col-lg-8 col-xl-7">
                                 <div className="page-heading">
-                                    <h1>Filmy Blog</h1>
-                                    <span className="subheading">Welcome to Loka...Chitraloka</span>
+                                    <h1>Blog App</h1>
+                                    <span className="subheading">Display Blogs</span>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </header>                <div className="container px-4 px-lg-5">
+                </header>
+                <div className="container px-4 px-lg-5">
                     <div className="row gx-4 gx-lg-5 justify-content-center">
                         <div className="col-md-10 col-lg-8 col-xl-7">
+                            {
+                                (this.state.blogs.length !== 0) && this.state.blogs.map((blog, index) =>
 
-                            <div className="d-flex justify-content-end mb-4"><Link className="btn btn-primary text-uppercase"
-                                to="/">Older Posts →</Link></div>
+                                    <div key={index}>
+                                        <div className="post-preview">
+                                            <Link to={`/blog/${blog._id}`}>
+                                                <h2 className="post-title">{blog.title}</h2>
+                                                <h3 className="post-subtitle">{blog.description}</h3>
+                                            </Link>
+                                            <p className="post-meta">
+                                                Posted by {blog.user.name}
+                                            </p>
+                                        </div>
+
+                                        <hr className="my-4" />
+                                    </div>
+                                )
+
+                            }
                         </div>
                     </div>
                 </div>
+
+
             </>
+
         )
     }
 }
